86e875079991a24e5406f850c42a468f581eff22
Othmane Achoual <oachoual@salesforce.com>
2020-09-30 07:59:17 +0100

    Updates

---
+++ js/forms.js
@@ -0,0 +1,8 @@
+$(function () {
+  $("#states").select2({
+    placeholder: "Please select",
+    minimumResultsForSearch: Infinity,
+    dropdownPosition: "below",
+    theme: "bootstrap4"
+  });
+});
